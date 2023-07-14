a803283332a42315a36f5195018ee9ca158595e0
2busynexus <112109292+2busynexus@users.noreply.github.com>
2023-07-14 13:30:26 +0000

    added connection to mongodb

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-  return "hello, flask+react+mongo happening here"
+# MongoDB connection setup
+uri = "mongodb+srv://2busymihai:testingdb@cluster0.qvu7rjj.mongodb.net/test?retryWrites=true&w=majority"
+cluster = MongoClient(uri)
+db = cluster['test']
+collection = db['test1']
 
-#mongodb+srv://2busymihai:<password>@cluster0.qvu7rjj.mongodb.net/?retryWrites=true&w=majority
+# API endpoint for retrieving data
+@app.route('/', methods=['GET'])
+def get_data():
+    result = list(collection.find())  # Convert the cursor object to a list
+
+    return jsonify(result)  # Convert the result to JSON format and return it
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+    app.run()
