7158494a51dea48198d919e4592bbdf2822c6ff6
2busynexus <112109292+2busynexus@users.noreply.github.com>
2023-07-26 19:37:33 +0000

    changed routes for adding, reading data from the new layout of my database

--- app.py
+++ app.py
@@ -16,60 +16,93 @@ app.config['SECRET_KEY'] = os.environ['secret']
 
 #confirm a successful connection
 try:
-    cluster.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
+  cluster.admin.command('ping')
+  print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
-    print(e)
+  print(e)
+
 
 def is_valid_user(userOrMail, password):
-    userToCheck = collection.find_one({"$or": [{"username": userOrMail}, {"email": userOrMail}]})
-    if userToCheck and userToCheck['password']==password:
-      print('OK')
-      return True
-    else:
-      print('FALSE')
-      return False
+  userToCheck = collection.find_one(
+    {"$or": [{
+      "username": userOrMail
+    }, {
+      "email": userOrMail
+    }]})
+  if userToCheck and userToCheck['password'] == password:
+    return True
+  else:
+    return False
 
 
 @app.route("/data", methods=['GET'])
-def get_data():
-    try:
-      result = collection.find()
-      data = []
-      for document in result:
-        document["_id"]=str(document["_id"])
-        data.append(document)
-      return jsonify(data)
-    except Exception as error:
-      return jsonify({"error": str(error)})
-      
+def data():
+  try:
+    user_id = session.get('user_id')
+    if not user_id:
+      return jsonify({"error": "User not logged in."})
+
+    user_id = ObjectId(user_id)
+    user = collection.find_one({"_id": user_id})
+    items = user['items']
+    #print(items)
+    itemsArray = []
+    for item in items:
+      itemsArray.append(item)
+    return jsonify(itemsArray)
+  except Exception as error:
+    return jsonify({"error": str(error)})
+
 
 @app.route("/add_data", methods=['POST'])
 def add_data():
   try:
+    #print("Add data route reached.")
+    user_id = session.get('user_id')
+    if not user_id:
+      return jsonify({"error": "User not logged in."})
+
     data = request.get_json()
-    newItem = {'name': data['name']}
-    result = collection.insert_one(newItem)
-    inserted_id = str(result.inserted_id)
-    newItem['_id'] = inserted_id
-    return jsonify(newItem)
+    #print("Received data:", data)
+    new_item = data['itemName']
+    #print("New item: ", new_item)
+    # Find the user in the database
+    user_id = ObjectId(user_id)
+    user = collection.find_one({"_id": user_id})
+
+    # Add the new item to the existing 'items' array and push it back into db
+    new_items_list = user['items']
+    new_items_list.append(new_item)
+    collection.update_one({"_id": ObjectId(user_id)},
+                          {'$set': {
+                            "items": new_items_list
+                          }})
+
+    return jsonify(new_item)
+
   except Exception as error:
     return jsonify({"error": str(error)})
-    
-  
-@app.route("/delete/<item_id>", methods=['DELETE'])
+
+
+@app.route("/delete/<item>", methods=['DELETE'])
 def delete(item_id):
-    try:
-        item_id = ObjectId(item_id)
-        result = collection.delete_one({"_id": item_id})
-        if result.deleted_count == 0:
-            print(f"Item with ID {item_id} not found.")
-            return jsonify({"error": "Item not found."})
-        print(f"Item with ID {item_id} deleted.")
-        return jsonify({"message": "Item deleted."})
-    except Exception as error:
-        print(f"Error deleting item: {str(error)}")
-        return jsonify({"error": str(error)})
+  try:
+    print("Delete data route reached")
+    user_id = session.get('user_id')
+    if not user_id:
+      return jsonify({"error": "User not logged in."})
+
+    item = request.get_json()
+    print(item)
+    result = collection.delete_one({"_id": item_id})
+    if result.deleted_count == 0:
+      print(f"Item with ID {item_id} not found.")
+      return jsonify({"error": "Item not found."})
+    #print(f"Item with ID {item_id} deleted.")
+    return jsonify({"message": "Item deleted."})
+  except Exception as error:
+    print(f"Error deleting item: {str(error)}")
+    return jsonify({"error": str(error)})
 
 
 @app.route('/register', methods=['POST'])
@@ -77,6 +110,7 @@ def register():
   try:
     data = request.get_json()
     user_data = data['user']
+
     newUser = {
       'username': user_data['username'],
       'email': user_data['email'],
@@ -86,11 +120,14 @@ def register():
     result = collection.insert_one(newUser)
     inserted_id = str(result.inserted_id)
     newUser['_id'] = inserted_id
-    print(f"{newUser['username']} added successfully.")
+    session['user_id'] = inserted_id
+    #print(session['user_id'])
+    #print(f"{newUser['username']} added successfully.")
     return jsonify(newUser)
   except Exception as error:
     print(f"Error: {str(error)}")
     return jsonify({'error': str(error)})
+
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -100,25 +137,44 @@ def login():
     userOrMail = user_data['userOrMail']
     password = user_data['password']
     if is_valid_user(userOrMail, password):
-            # If user is valid, return a success message or anything you want
-      return jsonify({'message': 'Login successful'})
+      userToCheck = collection.find_one(
+        {"$or": [{
+          "username": userOrMail
+        }, {
+          "email": userOrMail
+        }]})
+      session['user_id'] = str(userToCheck['_id'])
+      return jsonify({'message': 'Login successful'}), 200
     else:
-            # If user is not valid, return an error message or anything you want
-      return jsonify({'error': 'Invalid user or password'})
-    return jsonify(user)
+      return jsonify({'error': 'Invalid user or password'}), 401
   except Exception as error:
     print(f'Error: {str(error)}')
-    return jsonify({'error': str(error)})    
+    return jsonify({'error': str(error)}), 500
+
 
 @app.route('/check_login_status', methods=['GET'])
 def check_login_status():
   try:
-    return jsonify(False)
+    user_id = session.get('user_id')
+    if user_id:
+      # print(user_id)
+      # User is logged in
+      return jsonify(True)
+    else:
+      #print(user_id)
+      # User is not logged in
+      return jsonify(False)
   except Exception as error:
-    print(f'Error: {str(error)}')
-    return jsonify({'error': str(error)})
-    
-    
+    return jsonify({"error": str(error)})
+
+
+@app.route('/logout', methods=['POST'])
+def logout():
+  try:
+    session.pop('user_id')
+    return jsonify({"logged_in": False})
+  except Exception as error:
+    return jsonify({"error": str(error)})
 
 
 if __name__ == "__main__":
